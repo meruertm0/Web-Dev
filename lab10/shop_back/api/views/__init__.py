2c35dd4481d77a030ff6146feab04c378688339e
azizamyrzakhanova24 <m_mukhtarkyzy@kbtu.kz>
2026-04-08 01:46:05 +0500

    Initial Commit

---
+++ lab10/shop_back/api/views/__init__.py
@@ -0,0 +1,11 @@
+
+# from .fbv import products_list, product_detail
+# from .cbv import ProductListAPIView, ProductDetailAPIView
+# from .mixins import ProductListAPIView, ProductDetailAPIView
+from .generics import (
+    ProductListAPIView, 
+    ProductDetailAPIView, 
+    CategoryListAPIView, 
+    CategoryDetailAPIView, 
+    CategoryProductsAPIView
+)
